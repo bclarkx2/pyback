4b8ca3189c3f2cc3f47397a3e6f48bd6c4fc7a8d
Brian Clark <bclarkx2@verizon.net>
2017-04-05 21:36:12 -0400

    Added SimpleUser implementation of User -- not complete

--- client/user.py
+++ client/user.py
@@ -48,11 +48,48 @@ class SimpleUser(User):
         self.save_locations = save_locations
         self.data_locations = data_locations
 
+##############################################################################
+# Get/set                                                                    #
+##############################################################################
+
     def get_id(self):
         return self.id
+
+    def get_data_locations(self):
+        return self.data_location
+
+    def add_data_location(self, data_location):
+        self.data_locations.append(data_location)
+
+    def remove_data_location(self, data_location_id):
+        SimpleUser.remove_by_attr(self.data_locations,
+                                  SimpleUser.get_id,
+                                  data_location_id)
 
     def get_save_locations(self):
         return self.save_locations
 
-    def get_data_locations(self):
-        return self.data_locations
+    def add_save_location(self, save_location):
+        self.save_locations.append(save_location)
+
+    def remove_save_location(self, save_location_id):
+        SimpleUser.remove_by_attr(self.save_locations,
+                                  SimpleUser.get_id,
+                                  save_location_id)
+
+##############################################################################
+# Meat                                                                       #
+##############################################################################
+
+    def backup(self, data_locations, save_locations):
+        pass
+
+##############################################################################
+# Utility                                                                    #
+##############################################################################
+
+    @staticmethod
+    def remove_by_attr(lst, attr_method, remove_this_attr):
+        for item in lst:
+            if item.attr_method() == remove_this_attr:
+                lst.remove(item)
