b0d430bc3ef248db7fc9c92cb7d06e57904c54f1
Brian Clark <bclarkx2@verizon.net>
2017-04-08 10:52:43 -0400

    Implemented LocalSaveLocation

--- client/savelocation.py
+++ client/savelocation.py
@@ -3,14 +3,43 @@
 # Imports                                                                    #
 ##############################################################################
 
+import os
+import shutil
 
 
 ##############################################################################
 # Base class                                                                 #
 ##############################################################################
 
-class SaveLocation(object):
-    """docstring for SaveLocation"""
+class SaveLocation(object):  # pragma: no cover
 
     def __init__(self):
-        pass
+        raise NotImplementedError("Implement Me!")
+
+
+##############################################################################
+# Implementations                                                            #
+##############################################################################
+
+class LocalSaveLocation(SaveLocation):
+
+    def __init__(self, save_path):
+        self.save_path = save_path
+
+    def get_save_path(self):
+        return self.save_path
+
+    def save(self, data_location):
+
+        source_path = data_location.get_path()
+        dest_name = data_location.get_name()
+        dest_path = os.path.join(self.get_save_path(), dest_name)
+
+        LocalSaveLocation.safe_copy(source_path, dest_path)
+
+    @staticmethod
+    def safe_copy(source_path, dest_path):
+
+        shutil.rmtree(dest_path)
+        if(os.path.isdir(source_path)):
+            shutil.copytree(source_path, dest_path)
---
+++ utests/client/test_savelocation.py
@@ -0,0 +1,100 @@
+
+##############################################################################
+# Imports                                                                    #
+##############################################################################
+
+import unittest
+import os
+
+import client
+from client.savelocation import LocalSaveLocation
+
+from unittest.mock import MagicMock, call, patch
+
+from utests import commontest
+
+
+##############################################################################
+# Constants                                                                  #
+##############################################################################
+
+name_1 = "name_1"
+save_path_1 = "/some/save/path"
+save_item = os.path.join(save_path_1, name_1)
+
+data_path_1 = "/some/data/path/pre_save_name_1"
+
+
+##############################################################################
+# TestCases                                                                  #
+##############################################################################
+
+class LocalSaveLocationTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.save_location_1 = LocalSaveLocation(save_path_1)
+
+    def test_getters_and_setters(self):
+
+        self.assertEqual(save_path_1, self.save_location_1.get_save_path())
+
+    def test_safe_copy_valid_dir(self):
+
+        shutil_mock = MagicMock()
+        path_mock = MagicMock()
+        path_mock.isdir.return_value = True
+
+        patches = [
+            ("shutil.rmtree",),
+            ("shutil.copytree", shutil_mock),
+            ("os.path", path_mock)
+        ]
+
+        with commontest.MultiPatch(patches):
+            LocalSaveLocation.safe_copy(data_path_1, save_item)
+
+        shutil_args = [
+            call(data_path_1, save_item)
+        ]
+
+        self.assertEqual(shutil_args, shutil_mock.call_args_list)
+
+    def test_safe_copy_invalid_dir(self):
+
+        shutil_mock = MagicMock()
+        path_mock = MagicMock()
+        path_mock.isdir.return_value = False
+
+        patches = [
+            ("shutil.rmtree",),
+            ("shutil.copytree", shutil_mock),
+            ("os.path", path_mock)
+        ]
+
+        with commontest.MultiPatch(patches):
+            LocalSaveLocation.safe_copy(data_path_1, save_item)
+
+        shutil_args = []
+
+        self.assertEqual(shutil_args, shutil_mock.call_args_list)
+
+    def test_save_nominal(self):
+
+        safe_copy_mock = MagicMock()
+
+        data_location_mock = MagicMock()
+        data_location_mock.get_name.return_value = name_1
+        data_location_mock.get_path.return_value = data_path_1
+
+        with patch("client.savelocation.LocalSaveLocation.safe_copy", safe_copy_mock):
+            self.save_location_1.save(data_location_mock)
+
+        expected_copy_args = [
+            call(data_path_1, save_item)
+        ]
+
+        self.assertEqual(expected_copy_args, safe_copy_mock.call_args_list)
+
+
+if __name__ == '__main__':
+    unittest.main()
